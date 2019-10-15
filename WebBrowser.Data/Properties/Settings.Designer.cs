bb928e60024dc374334c1871f7a329aa8efe8396
Erika Yardumian <eyardumian@hotmail.com>
2019-10-15 10:23:35 -0700

    Created Bookmarks dataset/tableadapter

--- WebBrowser.Data/Properties/Settings.Designer.cs
+++ WebBrowser.Data/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace WebBrowser.Data.Properties {
                 return ((string)(this["HistoryDatabaseConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\BookmarksData" +
+            "base.mdf;Integrated Security=True")]
+        public string BookmarksDatabaseConnectionString {
+            get {
+                return ((string)(this["BookmarksDatabaseConnectionString"]));
+            }
+        }
     }
 }
